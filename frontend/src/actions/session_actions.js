1ff0ea0a11741dcbeb73454b0b2d27abbb37e849
MichaelPowell <gieringmj@gmail.com>
2020-02-11 09:23:36 -0800

    Add validations and model for groups

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -5,25 +5,20 @@ export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 
-// hello uwu change again
-// We'll dispatch this when our user signs in
 export const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
   currentUser
 });
 
-// We dispatch this one to show authentication errors on the frontend
 export const receiveErrors = errors => ({
   type: RECEIVE_SESSION_ERRORS,
   errors
 });
 
-// When our user is logged out, we will dispatch this action to set isAuthenticated to false
 export const logoutUser = () => ({
   type: RECEIVE_USER_LOGOUT
 });
 
-// Upon signup, dispatch the approporiate action depending on which type of response we receieve from the backend
 export const signup = user => dispatch => (
   APIUtil.signup(user).then(() => (
     dispatch(login(user))
@@ -32,7 +27,6 @@ export const signup = user => dispatch => (
   ))
 );
 
-// Upon login, set the session token and dispatch the current user. Dispatch errors on failure.
 export const login = user => dispatch => (
   APIUtil.login(user).then(res => {
     const { token } = res.data;
@@ -46,7 +40,6 @@ export const login = user => dispatch => (
     })
 )
 
-// We wrote this one earlier
 export const logout = () => dispatch => {
   localStorage.removeItem('jwtToken')
   APIUtil.setAuthToken(false)
