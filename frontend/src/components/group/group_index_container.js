acd595cb533af97dc50b8268254282b084be909d
misheMatcha <mishe.vong@gmail.com>
2020-02-11 11:16:14 -0800

    update group index container

--- frontend/src/components/group/group_index_container.js
+++ frontend/src/components/group/group_index_container.js
@@ -1,8 +1,14 @@
 import { connect } from 'react-redux';
 import GroupIndex from './group_index';
+import { requestAllGroups } from '../../actions/group_actions';
 
-const mapStateToProps = state = ({});
+const mapStateToProps = state = ({
+  // placeholder until db is seeded
+  groups: ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran ♀', 'Nidorina', 'Nidoqueen', 'Nidoran ♂', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff']
+});
 
-const mapDispatchToProps = dispatch = ({});
+const mapDispatchToProps = dispatch = ({
+  fetchAllGroups: () => dispatch(requestAllGroups())
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(GroupIndex);
--- frontend/src/reducers/groups_reducer.js
+++ frontend/src/reducers/groups_reducer.js
@@ -5,8 +5,6 @@ const initialState = {};
 export default function (state = initialState, action) {
   Object.freeze(state);
   switch (action.type) {
-    case RECEIVE_GROUP:
-      return {};
     case RECEIVE_ALL_GROUPS:
       return Object.assign({}, state, action.groups);
     default:
