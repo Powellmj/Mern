7cfae50c90a4b661f1a1c5f24a14563c73b7abc0
misheMatcha <mishe.vong@gmail.com>
2020-02-11 10:07:01 -0800

    add group api index route

---
+++ frontend/src/components/group/group_index_container.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import GroupIndex from './group_index';
+
+const mapStateToProps = state = ({});
+
+const mapDispatchToProps = dispatch = ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(GroupIndex);
--- routes/api/groups.js
+++ routes/api/groups.js
@@ -3,4 +3,10 @@ const router = express.Router();
 
 router.get("/test", (req, res) => res.json({ msg: "This is the groups route" }));
 
+router.get("/", (req, res) => {
+  Group.find()
+    .then(groups => res.json(groups))
+    .catch(err => res.status(404).json({ nogroupsfound: "No groups found" }))
+});
+
 module.exports = router;
