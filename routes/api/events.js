9274b6c1574bb35f8289651547e40e5ac501d24e
misheMatcha <mishe.vong@gmail.com>
2020-02-12 08:58:54 -0800

    add event index api route

--- routes/api/events.js
+++ routes/api/events.js
@@ -3,4 +3,10 @@ const router = express.Router();
 
 router.get("/test", (req, res) => res.json({ msg: "This is the events route" }));
 
+router.get("/", (req, res) => {
+  Event.find()
+    .then(events => res.json(events))
+    .catch(err => res.status(404).json({ noeventsfound: "No events found" }))
+});
+
 module.exports = router;
