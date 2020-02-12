ea29033e55f50e365ee89a4c8636c981b038d7ac
misheMatcha <mishe.vong@gmail.com>
2020-02-12 08:34:49 -0800

    add event model

---
+++ models/Event.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  desc: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  group_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'groups'
+  },
+  picture_id: {
+    type: String,
+    required: false
+  },
+  event_date: {
+    type: Date,
+    required: true
+  },
+  start_time: {
+    type: String,
+    required: true
+  },
+  end_time: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Group = mongoose.model('Group', GroupSchema);
