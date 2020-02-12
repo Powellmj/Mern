a9618b61614d0a075ef0f4aef5b31a5220fa973d
misheMatcha <mishe.vong@gmail.com>
2020-02-12 09:36:29 -0800

    add event errors reducer. update errors reducer

--- frontend/src/actions/event_actions.js
+++ frontend/src/actions/event_actions.js
@@ -1,10 +1,16 @@
 import * as EventAPIUtil from '../util/event_api_util';
 
 export const RECEIVE_ALL_EVENTS = 'RECEIVE_ALL_EVENTS';
+export const RECEIVE_EVENT_ERRORS = 'RECEIVE_EVENT_ERRORS';
 
 const receiveAllEvents = events => ({
   type: RECEIVE_ALL_EVENTS,
   events
+});
+
+const receiveEventErrors = errors => ({
+  type: RECEIVE_EVENT_ERRORS,
+  errors
 });
 
 export const requestAllEvents = () => dispatch => EventAPIUtil.fetchAllEvents()
