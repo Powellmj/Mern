7ee5c013ec47ef4a5955074339106429f06f2026
misheMatcha <mishe.vong@gmail.com>
2020-02-13 08:03:53 -0800

    update event form

--- frontend/src/components/event/event_form_container.js
+++ frontend/src/components/event/event_form_container.js
@@ -1,13 +1,16 @@
 import { connect } from 'react-redux';
 import { requestEvent } from '../../actions/event_actions';
+import { requestGroup } from '../../actions/group_actions';
 import EventForm from './event_form';
+import { fetchGroup } from '../../util/group_api_util';
 
 const mapStateToProps = state => ({
-  event: 'placeholder'
+  group: state.entities.groups.currentGroup
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchEvent: event => dispatch(requestEvent(event))
+  createEvent: event => dispatch(requestEvent(event)),
+  fetchGroup: group => dispatch(requestGroup(group))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventForm);
