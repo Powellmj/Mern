a5ffcd6632a94c094a25faa291c918383e6216cd
MichaelPowell <gieringmj@gmail.com>
2020-02-12 20:58:43 -0800

    fix

--- app.js
+++ app.js
@@ -23,13 +23,14 @@ app.use("/api/users", users);
 app.use("/api/groups", groups);
 app.use("/api/events", events);
 app.use(express.static(path.join(__dirname, 'public')));
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
