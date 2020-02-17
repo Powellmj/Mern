fa6ee9329f365c0b32fb15e6f420dc13f486e456
max821023 <max821023@gmail.com>
2020-02-17 12:48:15 -0800

    Add profile photo rerender

--- config/seeds.js
+++ config/seeds.js
@@ -9,22 +9,25 @@ mongoose
   .then(() => console.log("Seeded successfully"))
   .catch(err => console.log(err));
 
-// new User({
-//   name: "doctor",
-//   email: "doctor@gmail.com",
-//   password: "password"
-// }).save();
-
 new Group({
-  title: "Seed",
+  title: "absolutely utmost newest Seed",
   desc: "seed desc",
   location: "online",
   organizer: "Seed master"
-}).save();
+}).save()
+  .then(group => {
+    new Event({
+      title: "seed event",
+      desc: "seed event description",
+      location: "soil",
+      group_id: group._id,
+      event_start: "2020-03-01",
+      event_end: new Date("December 15, 2021")
+    }).save()
+      .then(event => {
+        group.events.push(event._id)
+        group.save()
+      })
+  }).then(console.log("done seeding"))
 
-new Event({
-  title: "seed event",
-  desc: "seed event description",
-  location: "soil",
-  
-})
+
